77b519801f4082e2a0de97d69fca01254bc0b957
stephmxrie <89794463+stephmxrie@users.noreply.github.com>
2022-02-12 21:52:23 -0800

    z
    z

---
+++ lab5/js/lab.js
@@ -0,0 +1,40 @@
+/* Hey we are learning JavaScript. Thank you Wes, very cool!
+
+  */
+
+// Define Variables
+make = "RX Hybrid";
+model = "Lexus";
+color = "Silver";
+year = "2020"
+
+
+// Calculate
+age = 2022 - year;
+
+
+// Output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age + " years<br>");
+
+
+
+
+/*
+carModel = "Lexus";
+'Lexus'
+console.log(carModel);
+VM2830:1 Lexus
+undefined
+carYear = 2020
+2020
+currentYear = 2021;
+2021
+carAge = currentYear - carYear;
+1
+console.log(carAge);
+VM3142:1 1 from google tab cred
+  */
